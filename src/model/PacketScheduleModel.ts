dd9b5b894d5923da392eed825667dd19ce936a64
billy.Lu <billy.lu@skylinetw.com>
2023-03-13 16:01:47 +0800

    feat:修正資料欄位錯誤

--- src/model/PacketScheduleModel.ts
+++ src/model/PacketScheduleModel.ts
@@ -10,8 +10,8 @@ export class PacketScheduleModel extends ModelBase {
         let mockDatas = await this.findAllData();
         let datas: MockerJsonData[] = [];
         for (let index = 0; index < mockDatas.length; index++) {
-            let { data } = mockDatas[index];
-            datas.push(data);
+            let { jsonData } = mockDatas[index];
+            datas.push(jsonData);
         }
         return datas;
     }
@@ -32,27 +32,29 @@ export class PacketScheduleModel extends ModelBase {
 
     public async getScheduleGameList(uid?: string) {
         let dbList = await this.getDBList();
-        let scheduleGameList = dbList.map(item => {
+        let scheduleGameList = dbList.map((item) => {
             let gameObj = getGameByName(item);
-            if(!gameObj) { return {} }
+            if (!gameObj) {
+                return {};
+            }
             const { CH, gameId } = gameObj;
             return {
                 name: CH,
-                id: gameId
-            }  
+                id: gameId,
+            };
         });
         return scheduleGameList;
     }
-   
+
     public async deleteMockData(uid: string, title: string) {
-        return await this.deleteData({ uid ,title});
+        return await this.deleteData({ uid, title });
     }
 
     public async updateMockData(uid: string, title: string, jsonData: any) {
-        return await this.updateData({uid,title},{ uid ,title, jsonData});
+        return await this.updateData({ uid, title }, { uid, title, jsonData });
     }
 
-    public async createMockData(uid: string, title: string,apply:boolean, jsonData: any) {
-        return await this.insertData({ uid ,title,apply,jsonData});
+    public async createMockData(uid: string, title: string, apply: boolean, jsonData: any) {
+        return await this.insertData({ uid, title, apply, jsonData });
     }
 }
