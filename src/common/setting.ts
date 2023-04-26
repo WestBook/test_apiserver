3320404a5af7821ec5244289f0cce0ea061f5daf
Sorrows <Sorrows.Lee@skylinetw.com>
2023-04-26 17:51:52 +0800

    調整成個人的設定

--- src/common/setting.ts
+++ src/common/setting.ts
@@ -2,7 +2,7 @@ const isDev = process.env.NODE_ENV === 'develop';
 const wsHost = isDev ? '127.0.0.1' : '192.168.1.158';
 const wsPort = isDev ? '27017' : '23456';
 const dbUrl = `mongodb://${wsHost}:${wsPort}`;
-const apiPort = 8087;
+const apiPort = 8084;
 const getLobbyUrl = (hostname: string) => {
     return isDev ? `http://${hostname}:8081` : 'http://192.168.1.158/lobby';
 };
