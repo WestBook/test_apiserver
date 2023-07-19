4a0a669a8629347e06e8bcf3f3e4ce07dc3628aa
billy.Lu <billy.lu@skylinetw.com>
2023-07-19 14:10:55 +0800

    feat:新增多語系大廳路徑API

--- src/common/setting.ts
+++ src/common/setting.ts
@@ -3,8 +3,11 @@ const wsHost = isDev ? '127.0.0.1' : '192.168.1.158';
 const wsPort = isDev ? '27017' : '23456';
 const dbUrl = `mongodb://${wsHost}:${wsPort}`;
 const apiPort = 8087;
-const getLobbyUrl = (hostname: string) => {
-    return isDev ? `http://${hostname}:8081` : 'http://192.168.1.158/lobby';
+const getLobbyUrl = (hostname: string, isMultiLang: boolean = false) => {
+    if (isDev) {
+        return `http://${hostname}:8081`;
+    }
+    return isMultiLang ? 'http://192.168.1.158/multiLangMockLobby' : 'http://192.168.1.158/lobby';
 };
 const slotGameType = {
     13: 'csd',
